acd4a8344a26d2b8ce92c0b1e83052c95ff2928f
brutaltea <brutaltea@bt.home.asus>
2018-07-12 01:50:32 +0300

    Style changed, edit added, show/hide bugs fixed

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
+//main.js v.0.0.1 for KB staging (12.07.18)
 $( document ).ready(function() {
-	$('.status').hover(function(){
-		$(this).dropdown('toggle');
-	}, function(){
-		$(this).dropdown('toggle');
+//Toggling Sections
+	$('.card-header').hover(function(){
+		$(this).next('.collapse').collapse('show');
+	},function(){
+		$(this).next('.collapse').addClass('collapseTriggered');
+		$('.collapseTriggered').mouseleave(function(){
+			$(this).collapse('hide');
+			$(this).removeClass('collapseTriggered');
+		})
 	});
 
-	$('.trigger1').hover(function(){
-		$('#collapseOne').collapse('toggle');
-	},function(){
-		$('#collapseOne').collapse('toggle');
-	})
-	$('.trigger2').hover(function(){
-		$('#collapseTwo').collapse('toggle');
-	},function(){
-		$('#collapseTwo').collapse('toggle');
-	})
-	$('.trigger3').hover(function(){
-		$('#collapseThree').collapse('toggle');
-	},function(){
-		$('#collapseThree').collapse('toggle');
-	})
+//Edititng sections
+	$('.section-edit-button').click(function(){
+		var button = $(this);
+		var cardBody = $(this).next();
+		$(this).css('display', 'none');
+		var text = $(this).next().text();
+		var newText;
+		$(this).next().html(`<textarea class="input-card-body">${text}</textarea>
+		<button class='section-save-button btn btn-success'>Save</button>`);
+		//this is just a simple JS function, you shouldnt mind this "this" below
+		$('.input-card-body').bind('input propertychange', function() {
+			newText = this.value;
+		  });
+		$('.section-save-button').click(function(){
+			cardBody.html(newText || text)
+			$(this).css('display', 'none');
+			button.css('display', 'block');
+
+		});
+	});
 });
